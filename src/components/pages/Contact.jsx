9131c0f81412a3e998a056433f0cc9079619ac9d
Abasay <abdulsalamasheem@gmail.com>
2023-09-16 08:01:44 +0000

    cleanups of irrelevant files and folders

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,139 +1,94 @@
 import React, { useState } from 'react'
 import { Client } from 'filestack-js'
 import * as filestack from 'filestack-js'
-import img from '../illustration_register.png'
 import { storageRef, database } from '../../firebase'
-
-// import { promises } from 'fs'
-// import path from 'path'
-// import process from 'process'
-// import { authenticate } from '@google-cloud/local-auth'
-// import { google } from 'googleapis'
-// import { readFile, writeFile } from 'fs'
+import '../home/landing.css'
+import './page.scss'
+import { MdSend } from 'react-icons/md'
+import logo from '../../images/Read A Book Motivational Quote Facebook Post.png'
+import Swal from 'sweetalert2'
 
 const Contact = () => {
-  const client_id =
-    '949071760081-4oi7544ujbkbp189u25n795n28umdldv.apps.googleusercontent.com'
-  const apiKey = 'AIzaSyCn66yVAJ6u8-JZEBaBV7eX2QB-SwnGhAo'
-
-  const client = filestack.init('ATGSFyd4ToOzaG39GOLprz')
-  const [file, setFiles] = useState(null)
-  const [upload, setUpload] = useState('')
-  const [imageArray, setImageArray] = useState([])
-  const [imgUrl, setImgUrl] = useState('')
-
-  const handleSelect = () => {
-    const input = document.createElement('input')
-    input.type = 'file'
-
-    input.onchange = (e) => {
-      let files = e.target.files
-      setFiles(files)
-      const reader = new FileReader()
-      reader.onload = function () {
-        return <img src={reader.src} alt='reader' />
-      }
-      reader.readAsDataURL(files[0])
-    }
-    input.click()
+  //Contact Page Component
+  const [details, setDetails] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    Swal.fire({
+      icon: 'success',
+      title: 'Success',
+      text: 'Message sent!!!',
+      confirmButtonColor: 'burlywood',
+    })
   }
 
-  const handleUpload = () => {
-    const imageName = 'newFile'
-    let uploadTask = storageRef.ref(`Images/${imageName}.png`).put(file[0])
-    uploadTask.on(
-      'state_changed',
-      function (snapshot) {
-        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        setUpload(`Upload ${progress}%`)
-      },
-      function (err) {
-        alert('error in uploading')
-      },
-      function () {
-        uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
-          const imgUrl = url
-          database.ref(`Pictures/${imageName}`).set({
-            Name: imageName,
-            Link: imgUrl,
-          })
-          alert('image added success')
-        })
-      }
-    )
-  }
-  const handleList = () => {
-    const newStorage = storageRef.ref('images')
-    console.log(newStorage.bucket)
-    let newArray = []
-
-    // newStorage
-    //   .getDownloadURL()
-    //   .then((url) => {
-    //     console.log(url)
-    //     // Insert url into an <img> tag to "download"
-    //   })
-    //   .catch((error) => {
-    //     // A full list of error codes is available at
-    //     // https://firebase.google.com/docs/storage/web/handle-errors
-    //     switch (error.code) {
-    //       case 'storage/object-not-found':
-    //         // File doesn't exist
-    //         break
-    //       case 'storage/unauthorized':
-    //         console.log('doesnt have permission to access the object')
-    //         break
-    //       case 'storage/canceled':
-    //         // User canceled the upload
-    //         break
-
-    //       // ...
-
-    //       case 'storage/unknown':
-    //         // Unknown error occurred, inspect the server response
-    //         break
-    //       default:
-    //         return 'good'
-    //     }
-    //   })
-    const fileUrls = newStorage
-      .listAll()
-      .then(function (res) {
-        res.items.map((image) => {
-          // And finally display them
-          displayImage(image)
-        })
-
-        
-      })
-      .catch(function (err) {
-        console.log(err)
-      })
-    function displayImage(imageRef) {
-      imageRef
-        .getDownloadURL()
-        .then(function (url) {
-          // TODO: Display the image on the UI
-          console.log(url)
-          newArray.push(url)
-          console.log(newArray)
-        })
-        .catch(function (error) {
-          // Handle any errors
-        })
-    }
-    console.log(newArray)
-  }
-  console.log(imageArray)
   return (
-    <div style={{ marginTop: '100px' }}>
-      {/* <button onClick={handleSelect}>select</button>
-      <button onClick={handleUpload}>upload</button> */}
-      <button onClick={handleList}>list</button>
-      <form action=''>
-        <label htmlFor=''>Upload file</label>
-        <input type='file' name='audio' id='' />
-      </form>
+    <div className='landing'>
+      <div className='landingpage'>
+        <img src={logo} width={'100%'} height={700} alt='' />
+      </div>
+      <div
+        className='contact-page'
+        style={{ margin: '20px auto', width: '80%' }}
+      >
+        <div className='page'>
+          <h3 style={{ display: 'block' }}>Send us a message</h3>
+          <form
+            action=''
+            method='post'
+            id='contact_form'
+            onSubmit={handleSubmit}
+          >
+            <input
+              type='text'
+              className='name'
+              placeholder='Full Name*'
+              required
+              onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            />
+            <input
+              type='email'
+              className='email'
+              placeholder='Email Address*'
+              onChange={(e) =>
+                setDetails({ ...details, email: e.target.value })
+              }
+              required
+            />
+
+            <input
+              type='text'
+              className='subject'
+              placeholder='Subject*'
+              required
+              onChange={(e) =>
+                setDetails({ ...details, subject: e.target.value })
+              }
+            />
+            <p style={{ fontWeight: 500 }}>Give us a message</p>
+            <textarea
+              name='msg'
+              id='msg'
+              cols='30'
+              rows='10'
+              required
+              placeholder='Message'
+              onChange={(e) =>
+                setDetails({ ...details, message: e.target.value })
+              }
+            ></textarea>
+
+            <button type='submit' className='send'>
+              send
+              <MdSend />
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/redux/reducers/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const LOGIN_SUCCESS = 'LOGIN SUCCESS'
-export const LOGIN_FAIL = 'LOGIN_FAIL'
-export const LOGIN_REQUEST = 'LOGIN_REQUEST'
-export const LOG_OUT = 'LOG OUT'
-export const LOAD_PROFILE = 'LOAD PROFILE'
