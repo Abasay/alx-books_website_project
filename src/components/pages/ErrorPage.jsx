655561d49c0a5de2cdfb9a68b98c8998f4eb1feb
Abasay <abdulsalamasheem@gmail.com>
2023-09-16 07:50:29 +0000

    Implementing a 404 error page for wrong routes

---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const ErrorPage = () => {
+  //Error Page for unknown or undefined routes
+  const navigate = useNavigate()
+  return (
+    <div
+      style={{
+        display: 'grid',
+        alignItems: 'center',
+        verticalAlign: 'center',
+        placeContent: 'center',
+        marginTop: '150px',
+      }}
+    >
+      404!! This page do not exist click{' '}
+      <span
+        onClick={() => navigate('/')}
+        style={{
+          textDecoration: 'underline',
+          color: 'burlywood',
+          cursor: 'pointer',
+        }}
+      >
+        here
+      </span>
+      to go back to home
+    </div>
+  )
+}
+
+export default ErrorPage
